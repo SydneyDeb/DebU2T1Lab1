1c21dd27ae6fd85e68972dd035bcfc2d26506f7f
Sydney Deb <sydneyd30@nycstudents.net>
2023-09-28 09:23:52 -0400

    added a parameter

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Cat {
+// Cat class definition
+public class Cat {
+
+    // instance variables
+    private String name;
+    private int age;
+    private double weight;
+    private String food;
+
+    // constructor
+    public Cat(String catName, int catAge, double catWeight, String favFood) {
+        name = catName;
+        age = catAge;
+        weight = catWeight;
+        food = favFood;
+    }
+
+    // method that introduces the Cat
+    public void introduce() {
+        if (age>7) {
+            System.out.println("Hello my name is " + name + " and I am an older cat.");
+        }
+        else {
+            System.out.println("Hello my name is " + name + "and I am a younger cat.");
+        }
+    }
+
+    // method that prints Cat info
+    public void printCatInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Fav Food: " + food);
+    }
 }
+
--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class CatRunner {
+public class CatRunner {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Shiney", 3, 3.5, "fish");
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Lily", 9, 5.5, "cat treats");
+        cat2.introduce();
+        cat2.printCatInfo();
+    }
 }
+
